3ff5d6892dcd5f05d711a820a092790f566b430e
蘇靖軒 <music1353@gmail.com>
2017-12-18 02:05:52 +0800

    修正path

--- personal/js/borrowing-page.js
+++ personal/js/borrowing-page.js
@@ -13,10 +13,31 @@ $(function () {
         // $(this).attr('id');
         console.log($(this));
         $('.return.modal').modal('show');
+
+        // ajax send comment
+        $.ajax({
+            url: ".aspx",
+            type: "POST",
+            data: {
+                'borrowerID': '借閱者ID',
+                'bookerID': '被借閱者ID',
+                'star': 'star-rate-value',
+                'feedback': 'comment'
+            },
+            success: function (data) {
+                // 評分完成後
+                $('#return-complete-btn').bind('click', function () {
+                    $('.return-complete.modal').modal('show');
+                });
+            },
+            error: function() {
+                // 錯誤
+            }
+        });
+
     });
 
     // rating init
-
     $('.ui.huge.star.rating').rating({
         initialRating: 3,
         maxRating: 5
--- personal/js/index-page.js
+++ personal/js/index-page.js
@@ -5,5 +5,13 @@ $(function(){
         // $(this).attr('id');
         console.log($(this));
         $('.borrow.modal').modal('show');
+        
+        // 確認借閱完成
+        $('#borrowModal-button').bind('click', function(){
+            console.log('借閱完成');
+            $('.borrow-complete.modal').modal('show');
+            
+            // 重新整理網頁
+        });
     });
 });
